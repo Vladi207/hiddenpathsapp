997b0baff6542b6aa4c80994049a50c409b4c794
Vladi207 <graschenko207@mail.ru>
2022-09-20 20:22:15 +0300

    Spring initializr

---
+++ src/main/java/com/netcracker/hiddenpathsapp/NetcrackerApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.hiddenpathsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/hiddenpathsapp/NetcrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.hiddenpathsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetcrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
